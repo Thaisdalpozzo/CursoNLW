1573306837fad42208b68d6a913e6477118d49e4
Thaís Da Rocha Dal Pozzo <66601480+Thaisdalpozzo@users.noreply.github.com>
2020-06-10 03:59:32 -0300

    commit inicial

---
+++ scripts/javascript.js
@@ -0,0 +1,61 @@
+// comments:
+// document.write("Hello")
+
+// variaveis: tipos de dados
+// var myvar= "Olá"
+// const myconst = "Olá"
+// document.write(myconst + myvar)
+
+// string:
+// const s1= "isso é uma string"
+// document.write(s1)
+
+// number: 
+// const n1 = 1
+// const n2 = 12
+// document.write(n1+n2)
+
+// bolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objetos; possuem propriedades e //funcionalidades;
+// const pessoa = {
+// altura: '1,63m',
+// idade: '21',
+// cabelo: true
+// correr(){
+// document.write('Run Forest')
+// }
+// }
+// pessoa.correr()
+
+// Arrey - Vetores (coleção de dados)
+// const colecaoDeBolinhas = ['blue' ,'green', 1, {name:'My Name'}]
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+// Registrar função:
+// function sayMyName(name) {
+  
+//  document.write(name)
+// }
+// executar
+// sayMyName('Thais,')
+// sayMyName('você está bem?')
+
+// Condicionais
+// const notaFinal = 3
+
+// if(notaFinal <5 ) {
+//  document.write('Reprovado')
+// } else {
+//  document.write('Aprovado')
+// }
+
+// loop/repetições
+
+// for (i=0; i < 10; i++) {
+//  document.write(`<p>${i}</p>`)
+// }
